fc7c893e6545788ca9ed8bc5be8d896a450f122b
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2023-12-26 10:19:54 +0700

    page: profile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        about: "url('/tailwindcss2/public/images/Hinh-san-pham-1.png')",
+        "footer-texture": "url('/img/footer-texture.png')",
+      },
+    },
   },
   plugins: [],
 };
