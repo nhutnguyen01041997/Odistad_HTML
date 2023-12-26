31001371a48e6eee5c26d8e11ac8c670345883a0
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2023-12-26 12:24:28 +0700

    update : css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      backgroundImage: {
-        about: "url('/tailwindcss2/public/images/Hinh-san-pham-1.png')",
-        "footer-texture": "url('/img/footer-texture.png')",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
