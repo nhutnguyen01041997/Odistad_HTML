948e5da561ab8e495397b2b4d900c867ac0d5585
nhutnguyen01041997 <72543308+nhutnguyen01041997@users.noreply.github.com>
2023-12-28 16:16:20 +0700

    page: weight detail

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "top-pattern": "url('../public/images/detail.png')",
+        "mid-pattern": "url('../public/images/bg-image2.png')",
+        "left-pattern": "url('../public/images/bg-image3.png')",
+        "right-pattern": "url('../public/images/bg-image4.png')",
+        "bottom-pattern": "url('../public/images/bg-image5.png')",
+      },
+    },
   },
-  plugins: [],
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".no-scrollbar::-webkit-scrollbar": {
+          display: "none",
+        },
+        ".no-scrollbar": {
+          "-ms-overflow-style": "none",
+          "scrollbar-width": "none",
+        },
+      };
+      addUtilities(newUtilities);
+    },
+  ],
 };
